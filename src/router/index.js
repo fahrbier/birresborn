e589837cb454ab3a4d852817f2cc4d95019aadae
hlersch <holger.lersch@factset.com>
2018-04-27 11:36:03 -0400

    the skeleton

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/views/HelloWorld'
+import Vereine from '@/views/Vereine'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/vereine/:id',
+      name: 'Vereine',
+      component: Vereine
+    }
+  ]
+})
---
+++ src/views/Vereine.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="row">
+        <div class="col">
+          <news :id="id" :headlines="headlines"/>
+        </div>
+        <div class="col">
+          2 of 2
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="row">
+        <div class="col">
+          1 of 3
+        </div>
+        <div class="col">
+          2 of 3
+        </div>
+        <div class="col">
+          3 of 3
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import news from '@/components/News'
+
+export default {
+  data: function () {
+    return {
+      id: '',
+      headlines: []
+    }
+  },
+  components: {
+    news
+  },
+  created: function () {
+    this.id = this.$route.params.id
+    var self = this
+    setTimeout(function () {
+      self.headlines = [{date: '2018-06-05', name: 'First'}, {date: '2018-06-05', name: 'Second'}, {date: '2018-06-05', name: 'Third'}]
+    }, 3000)
+  }
+}
+
+</script>
+
+<style>
+</style>
